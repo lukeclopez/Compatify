70cc06071cd98e5ff4f8f9229d75caf6aba5434b
Luke Lopez <xlucas14@gmail.com>
2019-10-22 13:27:29 -0400

    Replaced Zeta stuff with React stuff

--- docs/testing2.js
+++ docs/testing2.js
@@ -1,6 +1,11 @@
 var apiBaseUrl, data, user_id;
+var userNameEl, ageEL, lengthEL, popularityEL;
 
 apiBaseUrl = 'http://localhost:8000/api/generate_profile/'
+userNameEl = document.getElementById('user-name')
+ageEL = document.getElementById('avg-track-age')
+lengthEL = document.getElementById('avg-track-length')
+popularityEL = document.getElementById('avg-track-popularity')
 
 async function create_profile () {
 
@@ -10,6 +15,10 @@ async function create_profile () {
     .then(res => res.json())
     .then(json => {
         console.log(json);
+        userNameEl.innerHTML = json.user_id
+        ageEL.innerHTML = json.avg_track_age
+        lengthEL.innerHTML = json.avg_track_length
+        popularityEL.innerHTML = json.avg_track_popularity
     });
 }
 

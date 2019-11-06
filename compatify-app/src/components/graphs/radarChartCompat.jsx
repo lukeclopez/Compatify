e19ba80b7fc149c1a01c7d4a756b038409740aea
Luke Lopez <xlucas14@gmail.com>
2019-11-06 10:22:35 -0500

    Radar chart in profile now displays in card

--- compatify-app/src/components/graphs/radarChartCompat.jsx
+++ compatify-app/src/components/graphs/radarChartCompat.jsx
@@ -55,20 +55,31 @@ class RadarChartCompat extends Component {
     const radarData = this.makeDataObject(data);
 
     return (
-      <RadarChart outerRadius={120} width={500} height={400} data={radarData}>
-        <PolarGrid />
-        <PolarAngleAxis dataKey="trait" />
-        <PolarRadiusAxis angle={30} domain={[0, 1]} />
-        <Radar
-          name={name}
-          dataKey="A"
-          stroke="#8884d8"
-          fill="#8884d8"
-          fillOpacity={0.6}
-        />
-        <Legend />
-        <Tooltip />
-      </RadarChart>
+      <div class="card text-white bg-dark mb-3">
+        <div class="card-body">
+          <p class="card-text">
+            <RadarChart
+              outerRadius={120}
+              width={500}
+              height={300}
+              data={radarData}
+            >
+              <PolarGrid stroke="#ffffff" />
+              <PolarAngleAxis dataKey="trait" stroke="#ffffff" />
+              <PolarRadiusAxis angle={18} domain={[0, 1]} />
+              <Radar
+                name={name}
+                dataKey="A"
+                stroke="#8884d8"
+                fill="#8884d8"
+                fillOpacity={0.6}
+              />
+              <Legend />
+              <Tooltip wrapperStyle={{ color: "black" }} />
+            </RadarChart>
+          </p>
+        </div>
+      </div>
     );
   }
 }

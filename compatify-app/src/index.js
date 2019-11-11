61ed58337cef44a343cf66a8c43618c99cfdf365
Luke Lopez <xlucas14@gmail.com>
2019-11-11 06:45:12 -0500

    Setup Sentry

--- compatify-app/src/index.js
+++ compatify-app/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import * as Sentry from "@sentry/browser";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+
+Sentry.init({
+  dsn: "https://c3a2313aa7994c4b80fbae9931738d29@sentry.io/1815389"
+});
 
 ReactDOM.render(
   <>

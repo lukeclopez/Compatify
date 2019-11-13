b552cb35ba3a54f90be08521b98b16f028e24e63
Luke Lopez <xlucas14@gmail.com>
2019-11-12 23:19:11 -0500

    Overhauled artists cards

--- compatify-app/src/components/artistsCards.jsx
+++ compatify-app/src/components/artistsCards.jsx
@@ -5,7 +5,8 @@ import CardColumn from "./common/cardColumn";
 const ArtistsCards = props => {
   const { titleText, artists } = props;
   const style = {
-    maxWidth: "50%",
+    maxWidth: "100%",
+    minWidth: "100%",
     height: "auto",
     display: "block",
     marginLeft: "auto",
@@ -16,26 +17,27 @@ const ArtistsCards = props => {
       {artists &&
         artists.map(a => {
           return (
-            <React.Fragment key={shortid.generate()}>
-              <div className="card text-white bg-dark mb-3">
-                <img
-                  src={a.images[0] && a.images[0].url}
-                  className="card-img-top"
-                  alt=""
-                  style={style}
-                />
+            <div key={shortid.generate()}>
+              <div className="card text-white text-center bg-transparent my-3">
+                {a.images[0] ? (
+                  <img src={a.images[0].url} alt="" style={style} />
+                ) : (
+                  <img src="/no-image.png" alt="" style={style} />
+                )}
                 <div className="card-body">
                   <h5 className="card-title">{a.name}</h5>
                   <p className="card-text">
                     <b>Popularity: </b>
                     {a.popularity}/100
                   </p>
-                  <p className="card-text">
-                    <b>Genres: </b>
-                    {a.genres.map(g => (
-                      <li key={g}>{g}</li>
-                    ))}
-                  </p>
+                  {a.genres[0] && (
+                    <p className="card-text">
+                      <b>Genres: </b>
+                      {a.genres.map(g => (
+                        <div key={g}>{g}</div>
+                      ))}
+                    </p>
+                  )}
                   <a
                     href={a.external_urls.spotify}
                     className="btn btn-secondary"
@@ -44,7 +46,7 @@ const ArtistsCards = props => {
                   </a>
                 </div>
               </div>
-            </React.Fragment>
+            </div>
           );
         })}
     </CardColumn>
